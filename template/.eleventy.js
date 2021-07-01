a3d3c2feb02a5f4e574cafdb14bb68c7a7b98f11
Automated <actions@users.noreply.github.com>
2021-07-01 22:40:46 +0000

    Latest data: Thu Jul  1 22:40:46 UTC 2021

---
+++ template/.eleventy.js
@@ -0,0 +1,38 @@
+const pluginTailwind = require('eleventy-plugin-tailwindcss');
+
+module.exports = (config) => {
+  config.addPlugin(pluginTailwind, {
+    src: 'src/assets/css/*'
+  });
+
+  config.setDataDeepMerge(true);
+
+  config.addPassthroughCopy('src/assets/img/**/*');
+  config.addPassthroughCopy({ 'src/posts/img/**/*': 'assets/img/' });
+
+  config.addPassthroughCopy('src/smart-health/img');
+
+  config.addWatchTarget("src/assets/js/");
+
+  config.addFilter('readableDate', require('./lib/filters/readableDate'));
+  config.addFilter('minifyJs', require('./lib/filters/minifyJs'));
+
+  config.addTransform('minifyHtml', require('./lib/transforms/minifyHtml'));
+
+  config.addCollection('posts', require('./lib/collections/posts'));
+  config.addCollection('tagList', require('./lib/collections/tagList'));
+  config.addCollection('pagedPosts', require('./lib/collections/pagedPosts'));
+  config.addCollection('pagedPostsByTag', require('./lib/collections/pagedPostsByTag'));
+
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist',
+      layouts: "_layouts"
+    },
+    // pathPrefix: "/subfolder/",
+    templateFormats: ['md', 'njk', 'html'],
+    dataTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk'
+  };
+};
---
+++ template/src/learning.md
@@ -0,0 +1,7 @@
+---
+layout: default
+title: Learning
+permalink: "learning.html"
+---
+
+[Complex Variables](/complex-variables/)
