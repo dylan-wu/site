b4bec81db110dce4eb17148c4ec1b53f381df5ae
Dylan Wu <6586811+dylan-wu@users.noreply.github.com>
2021-07-01 19:56:32 -0500

    Update update.sh

--- src/update.sh
+++ src/update.sh
@@ -1,5 +1,5 @@
-yes | cp data/*.json template/src/_data
-yes | cp -r pages/* template/src/
-yes | rm -rf template/src/posts
-yes | cp -r  posts template/src/posts
-cat template-netlify.toml >> template/netlify.toml
+# yes | cp data/*.json template/src/_data
+# yes | cp -r pages/* template/src/
+# yes | rm -rf template/src/posts
+# yes | cp -r  posts template/src/posts
+# cat template-netlify.toml >> template/netlify.toml
