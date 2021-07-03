d5b3117f9d83b9adb54115f4445f6776eeb06107
Gary Wu <dylanwu@live.com>
2021-07-03 08:02:37 -0500

    shopt globstar

--- src/update.sh
+++ src/update.sh
@@ -1,5 +1,6 @@
 yes | cp data/*.json template/src/_data
 yes | cp -r pages/* template/src/
 yes | cp -r  posts/* template/src/posts/
+shopt -s globstar
 yes | cp -p posts/{**/*,*} template/src/posts/
 cat template-netlify.toml >> template/netlify.toml
