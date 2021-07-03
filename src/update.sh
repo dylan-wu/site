afe18a095284f68d9e74b9f281204cd3147902b3
Gary Wu <dylanwu@live.com>
2021-07-03 08:38:16 -0500

    fix post copy

--- src/update.sh
+++ src/update.sh
@@ -1,6 +1,5 @@
 yes | cp data/*.json template/src/_data
 yes | cp -r pages/* template/src/
-yes | cp -r  posts/* template/src/posts/
 shopt -s globstar
-yes | cp -p posts/{**/*,*} template/src/posts/
+yes | cp posts/{**/*,*} template/src/posts/
 cat template-netlify.toml >> template/netlify.toml
