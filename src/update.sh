b6d2e78c82a49958add743b578475a2e41b0e4f6
Gary Wu <dylanwu@live.com>
2021-07-03 18:01:35 -0500

    update.sh

--- src/update.sh
+++ src/update.sh
@@ -1,5 +1,12 @@
 yes | cp data/*.json template/src/_data
 yes | cp -r pages/* template/src/
-shopt -s globstar
-yes | cp posts/{**/*,*} template/src/posts/
 cat template-netlify.toml >> template/netlify.toml
+shopt -s globstar
+for file in posts/**/*;
+do
+    if [ -f "$file" ]; then
+        folder="$(dirname $file)"
+        cp -r "$folder" template/src/blog/
+    fi
+done
+
